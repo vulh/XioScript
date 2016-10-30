42bf5b96ef8ee5db1bcb6ef09104b64848531736
safebuster <safebuster2@gmail.com>
2016-10-30 12:40:53 +0300

    insufficient reserves in store

--- XS12.user.js
+++ XS12.user.js
@@ -2,14 +2,14 @@
 // @name           XioScript
 // @namespace      https://github.com/XiozZe/XioScript
 // @description    XioScript with XioMaintenance
-// @version        12.0.52
+// @version        12.0.53
 // @author		   XiozZe
 // @require        https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js
 // @include        http*://*virtonomic*.*/*/*
 // @exclude        http*://virtonomics.wikia.com*
 // ==/UserScript==
 
-var version = "12.0.52";
+var version = "12.0.53";
 
 /*
 
