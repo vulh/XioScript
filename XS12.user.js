1e0d8d73297edf4c53854fba0f3e92e5ea0ad074
vulh <vulh@vulh-W650EH>
2018-02-04 21:53:12 +0300

    Смена версии

--- XS12.user.js
+++ XS12.user.js
@@ -2,14 +2,14 @@
 // @name           XioScript
 // @namespace      https://github.com/XiozZe/XioScript
 // @description    XioScript with XioMaintenance
-// @version        12.0.127
+// @version        12.0.128
 // @author		   XiozZe
 // @require        https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js
 // @include        http*://*virtonomic*.*/*/*
 // @exclude        http*://virtonomics.wikia.com*
 // ==/UserScript==
 
-var version = "12.0.127";
+var version = "12.0.128";
 
 this.$ = this.jQuery = jQuery.noConflict(true);
 
